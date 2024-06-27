9639162b3d0da49eb9b6bfae574f2b51213d2e1c
TakeuchiTomoya1126 <takeuchi.tomoya@ithink.co.jp>
2024-06-27 11:44:04 +0900

    add sourcefile

---
+++ LastSurprise.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+from PIL import ImageGrab
+def Judge_Matching(num):
+    if 0.90 < num:
+        return True
+    else:
+        return False
+
+
+template = cv2.imread('./persona.png') #検索元画像ファイルパスを指定
+
+
+while(True):
+    ImageGrab.grab().save('./persona2.png')
+    image = cv2.imread('./persona2.png') #検索先画像のファイルパスを指定
+    #OpenCVで画像部分一致を検索
+    result = cv2.matchTemplate(image, template, cv2.TM_CCORR_NORMED)
+
+    # 最も類似度が高い位置と低い位置を取得します
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
+
+    #類似度が閾値を超えているか判定（上で作った関数を使用）
+    Judg = Judge_Matching(maxVal)
+    print(maxVal)
+    #結果を出力
+    if Judg:
+        print("true")
+    else:
+        print("false")
+    # （実行結果→）True or False
+    time.sleep(1)
